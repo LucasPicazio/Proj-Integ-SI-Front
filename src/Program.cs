cf3fb05c446fcb81a193cf156cda963d72b9fbc8
Hyago Quevedo <hyquec@gmail.com>
2020-12-09 21:02:43 -0300

    Alteração do BaseAddress por ambiente

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PSI_FRONT.Services;
 
@@ -14,13 +15,13 @@ namespace PSI_FRONT
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            ConfigureServices(builder.Services);
+            //builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            ConfigureServices(builder.Services, builder.Configuration.GetValue<string>("BaseURL"));
 
             await builder.Build().RunAsync();
         }
 
-        private static void ConfigureServices(IServiceCollection services)
+        private static void ConfigureServices(IServiceCollection services, string baseAddress)
         {
             services.AddAuthorizationCore();
             services.AddScoped<UserService>();
@@ -34,9 +35,8 @@ namespace PSI_FRONT
 
             services.AddSingleton(new HttpClient
             {
-                BaseAddress = new Uri("http://localhost:8181/")
-            }
-);
+                BaseAddress = new Uri(baseAddress)}
+            );
         }
     }
 }
